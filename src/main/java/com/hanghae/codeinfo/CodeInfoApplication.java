c48588da136c1059750f61bb1b0930762b551346
naocya@naver.com <naocya@naver.com>
2021-11-16 15:13:03 +0900

    first

---
+++ src/main/java/com/hanghae/codeinfo/CodeInfoApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.codeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/codeinfo/CodeInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.codeinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
