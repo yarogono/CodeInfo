67657333194b76aee8374526d9eeaec34c68d4da
naocya@naver.com <naocya@naver.com>
2021-11-20 22:18:35 +0900

    회원가입 폼

--- src/main/java/com/hanghae/codeinfo/controller/UserController.java
+++ src/main/java/com/hanghae/codeinfo/controller/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class UserController {
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/join")
+    public String joinPage() {
+        return "join";
     }
 }
