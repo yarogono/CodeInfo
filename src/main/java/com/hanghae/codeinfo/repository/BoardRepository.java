6c5c366a981e336226373384c9977aef45657ee9
naocya@naver.com <naocya@naver.com>
2022-04-27 21:30:07 +0900

    bugfix/boardRepository jpql버그

--- src/main/java/com/hanghae/codeinfo/repository/BoardRepository.java
+++ src/main/java/com/hanghae/codeinfo/repository/BoardRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Modifying
-    @Query("update Board b set b.title = :title b.content = :content where b.boardId = :id")
+    @Query("update Board b set b.title = :title, b.content = :content where b.postId = :id")
     void updateBoard(@Param("id") Long id,
                     @Param("title") String title,
                     @Param("content") String content);
