6c507ea2668ebffec19a176aaca2e3c259a5322d
yarogono <naocya@naver.com>
2022-05-01 00:13:10 +0900

    test/BoardServiceTest Assertions추가

--- src/test/java/com/hanghae/codeinfo/service/BoardServiceTest.java
+++ src/test/java/com/hanghae/codeinfo/service/BoardServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import javax.transaction.Transactional;
 
 @Transactional
