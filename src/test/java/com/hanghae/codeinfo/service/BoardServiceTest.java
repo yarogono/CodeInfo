23fd03565501e9e3e72a5f93a52bec002b0d8b03
yarogono <naocya@naver.com>
2022-05-03 16:10:46 +0900

    refact/BoardService 어노테이션 정리

--- src/test/java/com/hanghae/codeinfo/service/BoardServiceTest.java
+++ src/test/java/com/hanghae/codeinfo/service/BoardServiceTest.java
@@ -12,14 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
-import javax.transaction.Transactional;
-
-@Transactional
-@Rollback
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class BoardServiceTest {
 
