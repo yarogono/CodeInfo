eb6256be92721d6a2bfaf3652533f2882246d2bf
yarogono <naocya@naver.com>
2022-05-25 23:17:53 +0900

    refact/GlobalException 제거

--- src/main/java/com/hanghae/codeinfo/controller/UserController.java
+++ src/main/java/com/hanghae/codeinfo/controller/UserController.java
@@ -11,8 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @Controller
 @RequiredArgsConstructor
 public class UserController {
